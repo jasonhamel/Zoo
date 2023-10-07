30d65be4a79bb7b984cc39c93705c12e443df662
Jason <github@jasongetsemail.com>
2023-10-06 21:13:05 -0400

    Add tests to TestAnimal.
    Add error handling to Animal. In a real app this error handling would be done at time of data entry (by the user). But I'm not planning a main method, so its in the class itself.
    
    The error handling in Animal can be massively simplified by making a method for each property and calling the same method at instantiation and in the respective setter. But I have to make some mistakes to demonstrate I know better, and not that I'm lucking into optimised code lol.

--- src/Animal.java
+++ src/Animal.java
@@ -8,12 +8,39 @@ public class Animal {
     private HashSet<String> physicalFeatures;
     private String threatLevel;
 
-    public Animal(String name, int weight, int cuteFactor, String personality, HashSet<String> physicalFeatures, String threatLevel) {
+    public Animal(String name, int weight, int cuteFactor, String personality, HashSet<String> physicalFeatures, String threatLevel) throws Exception {
+        /*
+        Error handling would be on user input, but because I'm not making a main method and not actually going to
+        collect input, I'll put error handling here instead.
+         */
+        if (name.isBlank() || name.isEmpty()) {
+            throw new Exception("Name cannot be blank");
+        }
         this.name = name;
+
+        if (weight <= 0) {
+            throw new Exception("Weight cannot be zero or a negative number");
+        }
         this.weight = weight;
+
+        if (cuteFactor > 10 || cuteFactor < 0) {
+            throw new Exception("Please enter a cute factor between 0 and 10");
+        }
         this.cuteFactor = cuteFactor;
+
+        if (personality.isBlank() || personality.isEmpty()) {
+            throw new Exception("Please enter a personality for the animal");
+        }
         this.personality = personality;
+
+        if (physicalFeatures.isEmpty()) {
+            throw new Exception("Please enter at least one identifying feature");
+        }
         this.physicalFeatures = physicalFeatures;
+
+        if(threatLevel.isBlank() || threatLevel.isEmpty()) {
+            throw new Exception("Please enter a threat level");
+        }
         this.threatLevel = threatLevel;
     }
 
@@ -41,31 +68,60 @@ public class Animal {
         return this.threatLevel;
     }
 
-    public void setName(String name) {
+    public void setName(String name) throws Exception {
+        if (name.isBlank() || name.isEmpty()) {
+            throw new Exception("Name cannot be blank");
+        }
         this.name = name;
     }
 
-    public void setWeight(int weight) {
+    public void setWeight(int weight) throws Exception {
+        if (weight <= 0) {
+            throw new Exception("Weight cannot be zero or a negative number");
+        }
         this.weight = weight;
     }
 
-    public void setCuteFactor(int cuteFactor) {
+    public void setCuteFactor(int cuteFactor) throws Exception {
+        if (cuteFactor > 10 || cuteFactor < 0) {
+            throw new Exception("Please enter a cute factor between 0 and 10");
+        }
         this.cuteFactor = cuteFactor;
     }
 
-    public void setPersonality(String personality) {
+    public void setPersonality(String personality) throws Exception {
+        if (personality.isBlank() || personality.isEmpty()) {
+            throw new Exception("Please enter a personality for the animal");
+        }
         this.personality = personality;
     }
 
-    public void addPhysicalFeature(String physicalFeature) {
+    public void addPhysicalFeature(String physicalFeature) throws Exception {
+        if (physicalFeature.isEmpty() || physicalFeature.isBlank()) {
+            throw new Exception("Please enter a new identifying feature to add");
+        }
+        if (physicalFeatures.contains(physicalFeature)) {
+            System.out.println("Already added. Took no action here");
+            return;
+        }
         this.physicalFeatures.add(physicalFeature);
     }
 
-    public void removePhysicalFeature(String physicalFeature) {
-        physicalFeatures.remove(physicalFeature);
+    public void removePhysicalFeature(String physicalFeature) throws Exception {
+        if (physicalFeature.isEmpty() || physicalFeature.isBlank()) {
+            throw new Exception("Please enter an identifying feature to remove");
+        }
+        if (physicalFeatures.contains(physicalFeature)) {
+            physicalFeatures.remove(physicalFeature);
+            return;
+        }
+        System.out.println("Feature not present. Took no action here");
     }
 
-    public void setThreatLevel(String threatLevel) {
+    public void setThreatLevel(String threatLevel) throws Exception {
+        if(threatLevel.isBlank() || threatLevel.isEmpty()) {
+            throw new Exception("Please enter a threat level");
+        }
         this.threatLevel = threatLevel;
     }
 }
--- tests/TestAnimal.java
+++ tests/TestAnimal.java
@@ -5,18 +5,88 @@ import java.util.HashSet;
 
 public class TestAnimal {
     Animal animal1;
-    Animal animal2;
     @Before
-    public void setup() {
-        animal1 = new Animal("Argo", 70, 10, "Cuddle monster", new HashSet<String>(){{
+    public void setup() throws Exception {
+        animal1 = new Animal("Argo", 70, 10, "Cuddle monster", new HashSet<>(){{
             add("short yellow fur");
             add("eats a whole box of cereal off the counter");
         }}, "10 to cereal, -infinite to all else");
     }
 
     @Test
-    public void testName() {
+    public void testGetName() {
         Assert.assertEquals("Argo", animal1.getName());
+    }
+
+    @Test
+    public void testGetWeight() {
+        Assert.assertEquals(70, animal1.getWeight());
+    }
+
+    @Test
+    public void testGetCuteFactor() {
+        Assert.assertEquals(10, animal1.getCuteFactor());
+    }
+
+    @Test
+    public void testGetPersonality() {
+        Assert.assertEquals("Cuddle monster", animal1.getPersonality());
+    }
+
+    @Test
+    public void testGetPhysicalFeatures() {
+        Assert.assertEquals(new HashSet<>(){{
+            add("short yellow fur");
+            add("eats a whole box of cereal off the counter");
+        }}, animal1.getPhysicalFeatures());
+    }
+
+    @Test
+    public void testGetThreatLevel() {
+        Assert.assertEquals("10 to cereal, -infinite to all else", animal1.getThreatLevel());
+    }
+
+    @Test
+    public void testSetName() throws Exception {
+        animal1.setName("Iris");
+        Assert.assertEquals("Iris", animal1.getName());
+    }
+
+    @Test
+    public void testSetWeight() throws Exception{
+        animal1.setWeight(70);
+        Assert.assertEquals(70, animal1.getWeight());
+    }
+
+    @Test
+    public void testSetCuteFactor() throws Exception{
+        animal1.setCuteFactor(9);
+        Assert.assertEquals(9, animal1.getCuteFactor());
+    }
+
+    @Test
+    public void testSetPersonality() throws Exception{
+        animal1.setPersonality("The best");
+        Assert.assertEquals("The best", animal1.getPersonality());
+    }
+
+    @Test
+    public void testAddPhysicalFeatures() throws Exception{
+        animal1.addPhysicalFeature("Crazy eyebrows");
+        Assert.assertTrue(animal1.getPhysicalFeatures().contains("Crazy eyebrows"));
+        animal1.addPhysicalFeature("Crazy eyebrows");
+    }
+
+    @Test
+    public void testRemovePhysicalFeatures() throws Exception{
+        animal1.removePhysicalFeature("short yellow fur");
+        Assert.assertFalse(animal1.getPhysicalFeatures().contains("short yellow fur"));
+        animal1.removePhysicalFeature("short yellow fur");
+    }
+    @Test
+    public void testSetThreatLevel() throws Exception{
+        animal1.setThreatLevel("midnight");
+        Assert.assertEquals("midnight", animal1.getThreatLevel());
     }
 }
 
