caa9856b3b439b136e32b90f65dacf224f171b94
Jason <github@jasongetsemail.com>
2023-10-09 20:43:42 -0400

    Complete Mammal and TestMammal Classes.

--- src/Mammal.java
+++ src/Mammal.java
@@ -1,2 +1,57 @@
-public class Mammal {
+import java.util.HashSet;
+
+public class Mammal extends Animal{
+    private String furOrHairColour;
+    private boolean pregnant;
+    private int numberOfLegs;
+
+    public Mammal(String furOrHairColour, boolean pregnant, int numberOfLegs, String name, int weight, int cuteFactor, String personality,
+                   HashSet<String> physicalFeatures, String threatLevel) throws Exception {
+        super(name, weight, cuteFactor, personality,
+                physicalFeatures, threatLevel);
+
+        validateFurOrHairColour(furOrHairColour);
+        this.furOrHairColour = furOrHairColour;
+
+        this.pregnant = pregnant;
+
+        validateNumberOfLegs(numberOfLegs);
+        this.numberOfLegs = numberOfLegs;
+    }
+
+    public String getFurOrHairColour() {
+        return furOrHairColour;
+    }
+
+    public void setFurOrHairColour(String furOrHairColour) {
+        this.furOrHairColour = furOrHairColour;
+    }
+
+    public boolean getPregnant() {
+        return pregnant;
+    }
+
+    public void setPregnant(boolean pregnant) {
+        this.pregnant = pregnant;
+    }
+
+    public int getNumberOfLegs() {
+        return numberOfLegs;
+    }
+
+    public void setNumberOfLegs(int numberOfLegs) {
+        this.numberOfLegs = numberOfLegs;
+    }
+
+    private void validateFurOrHairColour(String furOrHairColour) throws Exception{
+        if (furOrHairColour.isEmpty() || furOrHairColour.isBlank()) {
+            throw new Exception("Fur/Hair colour cannot be blank");
+        }
+    }
+
+    private void validateNumberOfLegs(int numberOfLegs) throws Exception {
+        if (numberOfLegs > 4 || numberOfLegs < 0) {
+            throw new Exception("Number of legs must be between 0 and 4");
+        }
+    }
 }
--- tests/TestMammal.java
+++ tests/TestMammal.java
@@ -1,2 +1,84 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+
 public class TestMammal {
+    Mammal mammal;
+
+    @Before
+    public void setup() throws Exception {
+        mammal = new Mammal("Brown", false, 4, "Smokey", 900, 3, "mean",
+                new HashSet<>(){{
+                    add("scarred face");
+                    add("black muzzle");
+                }}, "midnight");
+    }
+
+    @Test
+    public void testGetFurOrHairColour() {
+        Assert.assertEquals("Brown", mammal.getFurOrHairColour());
+    }
+
+    @Test
+    public void testGetPregnant() {
+        Assert.assertFalse(mammal.getPregnant());
+    }
+
+    @Test
+    public void testGetNumberOfLegs() {
+        Assert.assertEquals(4, mammal.getNumberOfLegs());
+    }
+
+    @Test
+    public void testGetName() {
+        Assert.assertEquals("Smokey", mammal.getName());
+    }
+
+    @Test
+    public void testGetWeight() {
+        Assert.assertEquals(900, mammal.getWeight());
+    }
+
+    @Test
+    public void testGetCuteFactor() {
+        Assert.assertEquals(3, mammal.getCuteFactor());
+    }
+
+    @Test
+    public void testGetPersonality() {
+        Assert.assertEquals("mean", mammal.getPersonality());
+    }
+
+    @Test
+    public void testGetPhysicalFeatures() {
+        Assert.assertEquals(new HashSet<>(){{
+            add("scarred face");
+            add("black muzzle");
+        }}, mammal.getPhysicalFeatures());
+    }
+
+    @Test
+    public void testGetThreatLevel() {
+        Assert.assertEquals("midnight", mammal.getThreatLevel());
+    }
+
+    @Test
+    public void testSetFurOrHairColour() {
+        mammal.setFurOrHairColour("black");
+        Assert.assertEquals("black", mammal.getFurOrHairColour());
+    }
+
+    @Test
+    public void testSetPregnant() {
+        mammal.setPregnant(true);
+        Assert.assertTrue(mammal.getPregnant());
+    }
+
+    @Test
+    public void testSetNumberOfLegs() {
+        mammal.setNumberOfLegs(3);
+        Assert.assertEquals(3, mammal.getNumberOfLegs());
+    }
 }
