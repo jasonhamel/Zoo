8e46e7482cfb47653846c5d77f68fb24597b1911
Jason <github@jasongetsemail.com>
2023-10-09 21:07:22 -0400

    Complete Reptile and TestReptile Classes.

--- src/Reptile.java
+++ src/Reptile.java
@@ -1,2 +1,63 @@
-public class Reptile {
+import java.util.HashSet;
+
+public class Reptile extends Animal{
+
+    private boolean poisonous;
+    private boolean venomous;
+
+    private boolean shell;
+
+    private String colour;
+
+    public Reptile(boolean poisonous, boolean venomous, boolean shell, String colour, String name, int weight, int cuteFactor, String personality,
+                   HashSet<String> physicalFeatures, String threatLevel) throws Exception {
+        super(name, weight, cuteFactor, personality,
+                physicalFeatures, threatLevel);
+
+        this.poisonous = poisonous;
+        this.venomous = venomous;
+        this.shell = shell;
+
+        validateColour(colour);
+        this.colour = colour;
+    }
+
+    public boolean getPoisonous() {
+        return poisonous;
+    }
+
+    public void setPoisonous(boolean poisonous) {
+        this.poisonous = poisonous;
+    }
+
+    public boolean getVenomous() {
+        return venomous;
+    }
+
+    public void setVenomous(boolean venomous) {
+        this.venomous = venomous;
+    }
+
+    public boolean getShell() {
+        return shell;
+    }
+
+    public void setShell(boolean shell) {
+        this.shell = shell;
+    }
+
+    public String getColour() throws Exception {
+        validateColour(colour);
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void validateColour(String colour) throws Exception{
+        if (colour.isBlank() || colour.isEmpty()) {
+            throw new Exception("Colour cannot be blank");
+        }
+    }
 }
--- tests/TestReptile.java
+++ tests/TestReptile.java
@@ -1,2 +1,96 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+
 public class TestReptile {
+
+    Reptile reptile;
+
+    @Before
+    public void setup() throws Exception {
+        reptile = new Reptile(false, false, true, "green", "Turtle Boy", 4, 3, "quiet",
+                new HashSet<>(){{
+                    add("yellow and brown spots on back of shell");
+                    add("smiles");
+                }}, "green");
+    }
+
+    @Test
+    public void testGetPoisonous() {
+        Assert.assertFalse(reptile.getPoisonous());
+    }
+
+    @Test
+    public void testGetVenomous() {
+        Assert.assertFalse(reptile.getVenomous());
+    }
+
+    @Test
+    public void testGetShell() {
+        Assert.assertTrue(reptile.getShell());
+    }
+
+    @Test
+    public void testGetColour() throws Exception {
+        Assert.assertEquals("green", reptile.getColour());
+    }
+
+    @Test
+    public void testGetName() {
+        Assert.assertEquals("Turtle Boy", reptile.getName());
+    }
+
+    @Test
+    public void testGetWeight() {
+        Assert.assertEquals(4, reptile.getWeight());
+    }
+
+    @Test
+    public void testGetCuteFactor() {
+        Assert.assertEquals(3, reptile.getCuteFactor());
+    }
+
+    @Test
+    public void testGetPersonality() {
+        Assert.assertEquals("quiet", reptile.getPersonality());
+    }
+
+    @Test
+    public void testGetPhysicalFeatures() {
+        Assert.assertEquals(new HashSet<>(){{
+            add("yellow and brown spots on back of shell");
+            add("smiles");
+        }}, reptile.getPhysicalFeatures());
+    }
+
+    @Test
+    public void testGetThreatLevel() {
+        Assert.assertEquals("green", reptile.getThreatLevel());
+    }
+
+    @Test
+    public void testSetPoisonous() {
+        reptile.setPoisonous(true);
+        Assert.assertTrue(reptile.getPoisonous());
+    }
+
+    @Test
+    public void testSetVenomous() {
+        reptile.setVenomous(true);
+        Assert.assertTrue(reptile.getVenomous());
+    }
+
+    @Test
+    public void testSetShell() {
+        reptile.setShell(false);
+        Assert.assertFalse(reptile.getShell());
+    }
+
+    @Test
+    public void testSetColour() throws Exception {
+        reptile.setColour("brown and red stripes");
+        Assert.assertEquals("brown and red stripes", reptile.getColour());
+    }
 }
