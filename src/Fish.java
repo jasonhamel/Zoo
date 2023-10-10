dfb92e3243258b6d404a3ebbe70352fbba62088f
Jason <github@jasongetsemail.com>
2023-10-09 20:10:29 -0400

    Complete Fish and TestFish Classes.

--- src/Fish.java
+++ src/Fish.java
@@ -1,2 +1,43 @@
-public class Fish {
+import java.util.HashSet;
+
+public class Fish extends Animal{
+
+    private String colourOfScales;
+    private String depth;
+
+    public Fish (String colourOfScales, String depth, String name, int weight, int cuteFactor, String personality,
+                 HashSet<String> physicalFeatures, String threatLevel) throws Exception {
+        super(name, weight, cuteFactor, personality,
+                physicalFeatures, threatLevel);
+
+        validateStrings(colourOfScales);
+        this.colourOfScales = colourOfScales;
+
+        validateStrings(depth);
+        this.depth = depth;
+    }
+
+    public String getColourOfScales() {
+        return colourOfScales;
+    }
+
+    public void setColourOfScales(String colourOfScales) throws Exception{
+        validateStrings(colourOfScales);
+        this.colourOfScales = colourOfScales;
+    }
+
+    public String getDepth() {
+        return depth;
+    }
+
+    public void setDepth(String depth) throws Exception {
+        validateStrings(depth);
+        this.depth = depth;
+    }
+
+    private void validateStrings(String string) throws Exception {
+        if (string.isBlank() || string.isEmpty()) {
+            throw new Exception("Colour of Scales and Depth cannot be blank");
+        }
+    }
 }
--- tests/TestFish.java
+++ tests/TestFish.java
@@ -1,2 +1,73 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+
 public class TestFish {
+
+    Fish fish;
+
+    @Before
+    public void setup() throws Exception {
+        fish = new Fish("red", "3 meters", "Bro", 1, 1, "none.",
+                new HashSet<>(){{
+                    add("black lines");
+                    add("yellow fins");}}, "none");
+    }
+
+    @Test
+    public void testGetColourOfScales() {
+        Assert.assertEquals("red", fish.getColourOfScales());
+    }
+
+    @Test
+    public void testGetDepth() {
+        Assert.assertEquals("3 meters", fish.getDepth());
+    }
+
+    @Test
+    public void testGetName() {
+        Assert.assertEquals("Bro", fish.getName());
+    }
+
+    @Test
+    public void testGetWeight() {
+        Assert.assertEquals(1, fish.getWeight());
+    }
+
+    @Test
+    public void testGetCuteFactor() {
+        Assert.assertEquals(1, fish.getCuteFactor());
+    }
+
+    @Test
+    public void testGetPersonality() {
+        Assert.assertEquals("none.", fish.getPersonality());
+    }
+
+    @Test
+    public void testGetPhysicalFeatures() {
+        Assert.assertEquals(new HashSet<>(){{
+            add("black lines");
+            add("yellow fins");
+        }}, fish.getPhysicalFeatures());
+    }
+
+    @Test
+    public void testGetThreatLevel() {
+        Assert.assertEquals("none", fish.getThreatLevel());
+    }
+
+    @Test
+    public void testSetColourOfScales() throws Exception {
+        fish.setColourOfScales("green");
+        Assert.assertEquals("green", fish.getColourOfScales());
+    }
+
+    @Test
+    public void testSetDepth() throws Exception {
+        fish.setDepth("10 meters");
+        Assert.assertEquals("10 meters", fish.getDepth());
+    }
 }
