30f225a4f41547b8a7717a9ab20635c1b1cd5276
Jason <github@jasongetsemail.com>
2023-10-15 21:14:32 -0400

    Begin adding logic for Zoo. This Class is being written using TDD and the TestZoo class. More functionality is needed. More testing of the remove method is needed. The remove and getFrom* methods can be optimised still.

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,3 +1,105 @@
+import java.util.HashSet;
+import java.util.Objects;
+
 public class Zoo {
+    private final HashSet<Fish> aquarium;
+    private final HashSet<Bird> aviary;
+    private final HashSet<Reptile> herpetarium;
+    private final HashSet<Mammal> pen;
+
+    public Zoo() {
+        aquarium = new HashSet<>();
+        aviary = new HashSet<>();
+        herpetarium = new HashSet<>();
+        pen = new HashSet<>();
+    }
+    public void addAquarium(Fish fish) {
+        aquarium.add(fish);
+    }
+
+    public void addAviary(Bird bird) {
+        aviary.add(bird);
+    }
+
+    public void addHerpetarium(Reptile reptile) {
+        herpetarium.add(reptile);
+    }
+
+    public void addPen(Mammal mammal) {
+        pen.add(mammal);
+    }
+
+    public HashSet<Fish> getAllFish() {
+        return aquarium;
+    }
+
+    public HashSet<Bird> getAllBirds() {
+        return aviary;
+    }
+
+    public HashSet<Reptile> getAllReptiles() {
+        return herpetarium;
+    }
+
+    public HashSet<Mammal> getAllMammals() {
+        return pen;
+    }
+
+    public Animal getOneFish(String name) {
+
+        return aquarium.stream()
+                    .filter(fish -> Objects.equals(fish.getName(), name))
+                    .findFirst()
+                    .orElse(null);
+    }
+
+    public Animal getOneBird(String name) {
+
+        return aviary.stream()
+                .filter(bird -> Objects.equals(bird.getName(), name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Animal getOneMammal(String name) {
+
+        return pen.stream()
+                .filter(mammal -> Objects.equals(mammal.getName(), name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Animal getOneReptile(String name) {
+
+        return herpetarium.stream()
+                .filter(reptile -> Objects.equals(reptile.getName(), name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void remove(Animal animal, String name) {
+        if (animal instanceof Bird) {
+            aviary.remove(aviary.stream()
+                    .filter(bird -> Objects.equals(bird.getName(), name))
+                    .findFirst()
+                    .orElse(null));
+        } else if (animal instanceof Fish) {
+            aquarium.remove(aquarium.stream()
+                    .filter(fish -> Objects.equals(fish.getName(), name))
+                    .findFirst()
+                    .orElse(null));
+        } else if (animal instanceof Mammal) {
+            pen.remove(pen.stream()
+                    .filter(mammal -> Objects.equals(mammal.getName(), name))
+                    .findFirst()
+                    .orElse(null));
+        } else {
+            herpetarium.remove(herpetarium.stream()
+                    .filter(reptile -> Objects.equals(reptile.getName(), name))
+                    .findFirst()
+                    .orElse(null));
+        }
+    }
+
 
 }
--- tests/TestZoo.java
+++ tests/TestZoo.java
@@ -1,2 +1,143 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.HashSet;
+
 public class TestZoo {
+
+    Zoo zoo;
+    Clownfish clownfish;
+    Eel eel;
+    Shark shark;
+    Cat cat;
+    Dog dog;
+    Giraffe giraffe;
+    Owl owl;
+    Penguin penguin;
+    Puffin puffin;
+    Chameleon chameleon;
+    Snake snake;
+    Turtle turtle;
+
+    @Before
+    public void set() throws Exception {
+        zoo = new Zoo();
+        clownfish = new Clownfish(true, "Orange", "reef deep", "Nemo", 1, 7,
+                "forgetful", new HashSet<>(){{add("white stripes");}}, "zero");
+        eel = new Eel(true, "red", "bottom", "Eel-ectric", 10, 0, "slippery",
+                new HashSet<>(){{add("chipped fangs");}}, "will eat you");
+        shark = new Shark(2, "blue", "500", "Sea Bear", 200, 3, "mean",
+                new HashSet<>(){{add("missing two middle teeth");}}, "midnight");
+        cat = new Cat(true, true, true, "brown", false, 4, "Iris",
+                10, 8, "bitchy", new HashSet<>(){{add("one eye");}}, "claws and teeth");
+        dog = new Dog(10, true, false, "yellow", false, 4, "Argo", 70, 10,"cuddles",
+                new HashSet<>(){{add("expressive eyebrows");}}, "none at all");
+        giraffe = new Giraffe(69, "green", "brown and yellow", true, 4, "Gira",
+                800, 5, "acts tall", new HashSet<>(){{add("missing tail");}}, "not much");
+        owl = new Owl(270, 40, true, false, "Hootsy", 20, 3, "night owl",
+                new HashSet<>(){{add("big spots in white feathers");}}, "silent but deadly");
+        penguin = new Penguin(true, 30, false, true, "Pingu", 40, 8, "formal",
+                new HashSet<>(){{add("winking all the time");}}, "don't stand at the bottom of the slippin slide");
+        puffin = new Puffin(true, 30, true, false, "Puff", 20, 10, "Endeering",
+                new HashSet<>(){{add("partner's feathers all over it");}}, "nope");
+        chameleon = new Chameleon(true, false, false, false, "all", "Cam", 3, 3,
+                "shy", new HashSet<>(){{add("impossible to tell");}}, "the bottom");
+        snake = new Snake(true, false, false, true, false, "brown", "Esses",
+                30, 0, "not much of a talker", new HashSet<>(){{add("tip of tail missing");}}, "terror");
+        turtle = new Turtle("fresh", false, false, true, "blue", "Torts", 50,
+                6, "slow", new HashSet<>(){{add("real long tail");}}, "nah bro");
+    }
+
+    @Test
+    public void testAddOneAquarium() {
+        zoo.addAquarium(eel);
+        Assert.assertTrue(zoo.getAllBirds().isEmpty());
+        Assert.assertTrue(zoo.getAllMammals().isEmpty());
+        Assert.assertTrue(zoo.getAllReptiles().isEmpty());
+        Assert.assertFalse(zoo.getAllFish().isEmpty());
+    }
+
+    @Test
+    public void testAddOneAviary() {
+        zoo.addAviary(penguin);
+        Assert.assertTrue(zoo.getAllMammals().isEmpty());
+        Assert.assertTrue(zoo.getAllReptiles().isEmpty());
+        Assert.assertTrue(zoo.getAllFish().isEmpty());
+        Assert.assertFalse(zoo.getAllBirds().isEmpty());
+    }
+
+    @Test
+    public void testAddOneHerpetarium() {
+        zoo.addHerpetarium(turtle);
+        Assert.assertTrue(zoo.getAllBirds().isEmpty());
+        Assert.assertTrue(zoo.getAllMammals().isEmpty());
+        Assert.assertTrue(zoo.getAllFish().isEmpty());
+        Assert.assertFalse(zoo.getAllReptiles().isEmpty());
+    }
+
+    @Test
+    public void testAddOnePen() {
+        zoo.addPen(dog);
+        Assert.assertTrue(zoo.getAllBirds().isEmpty());
+        Assert.assertTrue(zoo.getAllReptiles().isEmpty());
+        Assert.assertTrue(zoo.getAllFish().isEmpty());
+        Assert.assertFalse(zoo.getAllMammals().isEmpty());
+    }
+
+    @Test
+    public void testGetFromAquarium() {
+        zoo.addAquarium(clownfish);
+        zoo.addAquarium(eel);
+        zoo.addAquarium(shark);
+        Assert.assertEquals("Nemo", zoo.getOneFish("Nemo").getName());
+        Assert.assertEquals("Eel-ectric", zoo.getOneFish("Eel-ectric").getName());
+        Assert.assertEquals("Sea Bear", zoo.getOneFish("Sea Bear").getName());
+        Assert.assertNull(zoo.getOneFish("Jason"));
+    }
+
+    @Test
+    public void testGetFromAviary() {
+        zoo.addAviary(owl);
+        zoo.addAviary(penguin);
+        zoo.addAviary(puffin);
+        Assert.assertEquals("Hootsy", zoo.getOneBird("Hootsy").getName());
+        Assert.assertEquals("Pingu", zoo.getOneBird("Pingu").getName());
+        Assert.assertEquals("Puff", zoo.getOneBird("Puff").getName());
+        Assert.assertNull(zoo.getOneBird("Jason"));
+    }
+
+    @Test
+    public void testGetFromHerpetarium() {
+        zoo.addHerpetarium(chameleon);
+        zoo.addHerpetarium(snake);
+        zoo.addHerpetarium(turtle);
+        Assert.assertEquals("Cam", zoo.getOneReptile("Cam").getName());
+        Assert.assertEquals("Esses", zoo.getOneReptile("Esses").getName());
+        Assert.assertEquals("Torts", zoo.getOneReptile("Torts").getName());
+        Assert.assertNull(zoo.getOneReptile("Jason"));
+    }
+
+    @Test
+    public void testGetFromPen() {
+        zoo.addPen(cat);
+        zoo.addPen(dog);
+        zoo.addPen(giraffe);
+        Assert.assertEquals("Iris", zoo.getOneMammal("Iris").getName());
+        Assert.assertEquals("Argo", zoo.getOneMammal("Argo").getName());
+        Assert.assertEquals("Gira", zoo.getOneMammal("Gira").getName());
+        Assert.assertNull(zoo.getOneMammal("Jason"));
+    }
+
+    @Test
+    public void testRemove() {
+        zoo.addPen(cat);
+        zoo.addPen(dog);
+        zoo.addAquarium(eel);
+        Assert.assertTrue(zoo.getAllBirds().isEmpty());
+        Assert.assertTrue(zoo.getAllReptiles().isEmpty());
+        zoo.remove(eel, eel.getName());
+        Assert.assertTrue(zoo.getAllFish().isEmpty());
+        zoo.remove(cat, cat.getName());
+        Assert.assertFalse(zoo.getAllMammals().isEmpty());
+    }
 }
